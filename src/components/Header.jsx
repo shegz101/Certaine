f12b1a2dcbd6095a43f6aef7f202f75d4a53d3a7
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-07-30 17:24:39 +0100

    added certaine logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import logo from '../logo.jpg';
 import '../style/Header.css';
 
 const Header = () => {
     return (
         <div className='header-section'> 
-            <div><h1 style={{color:'white'}} className='header-text'>Certaine</h1></div>
+            <div className='header__logo'>
+                <span><img src={logo} style={{fontSize:'20px',height:'28px',marginTop:'20px',lineHeight:'2.4',}}alt="certaine-logo"/></span>
+                <h1 style={{color:'white', paddingTop:'20px', paddingLeft:'10px'}} className='header-text'>Certaine</h1>
+            </div>
             <div className='button-grp'>
                 <button className='init-button'>Init Account</button>
                 <button className='connect-button'>Connect to Wallet</button>
