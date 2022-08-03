7508bebb527ba943cf50e1a5f2a42c10e5947ae2
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-08-03 12:15:52 +0100

    sign-up-page-takes-new-look

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,9 +5,6 @@ import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <div className="login-section">
-            <div className='login__image'>
-            <Link to="/" style={{textDecoration:'none',}}><p className='back__history__text' style={{color:'white', paddingLeft:'30px', paddingTop:'30px', fontSize:'38px', fontWeight:'400', wordSpacing:'20', textDecoration:'none',}}>Certaine</p></Link>
-            </div>
             <div className='login__input'>
                <h1 className="sign__text">Sign Up</h1>
                <input type='text' placeholder='Name' className="name__field"/>
