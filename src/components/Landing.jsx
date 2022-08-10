a678d7040844e188a37a709a46ca19084e566eae
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-08-10 13:26:25 +0100

    description-text-aligns

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -208,7 +208,7 @@ const Landing = (props) => {
            <div className='main__landing__section'>
               <div className='landing__text'>
                   <h1 style={{color:'white', fontSize:'55px', width:'100%'}}>Make instant NFT Certificate Builder Platform</h1>
-                  <p className='descrp__txt' style={{color:'hsla(0,0%,100%,.5)', width:'600px',}}>It takes seconds to upload certificate and create an NFT certificate on Certaine. Easily Connect your Wallet. Send NFT to any address.</p>
+                  <p className='descrp__txt' style={{color:'hsla(0,0%,100%,.5)', width:'100%',}}>It takes seconds to upload certificate and create an NFT certificate on Certaine. Easily Connect <span>your Wallet. Send NFT to any address</span>.</p>
                   <div>
                     <button className='new__button' onClick={() => setSignModal(true)} style={{marginTop:'20px', border:'1px solid transparent'}}>New to Certaine</button>
                     <button onClick={() => setExplore(true)} className='sign-button' style={{marginTop:'20px', width:'8rem', marginLeft:'4rem', border:'1px solid transparent'}}>Explore</button>
